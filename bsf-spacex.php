7bac7a01b0f56e9ef4bbbf2cdf671c1c84be733b
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-02-05 08:06:38 +0600

    🎉 chore: Composer setup with plugin info

---
+++ bsf-spacex.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       BSF-Spacex Tests
+ * Description:       WordPress plugin for Fullstack WordPress Development test
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Version:           0.0.1
+ * Tested upto:       6.1.1
+ * Author:            Maniruzzaman Akash<manirujjamanakash@gmail.com>
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bsf-spacex
+ */
